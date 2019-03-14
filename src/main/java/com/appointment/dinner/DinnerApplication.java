ee80a77283a3f5f2ace61452d388041584fefd30
Dzc96 <546448917@qq.com>
2019-03-14 12:14:37 +0800

    图片上传测试-4

--- src/main/java/com/appointment/dinner/DinnerApplication.java
+++ src/main/java/com/appointment/dinner/DinnerApplication.java
@@ -3,6 +3,10 @@ package com.appointment.dinner;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @MapperScan("com.appointment.dinner.**.mapper")
@@ -11,6 +15,17 @@ public class DinnerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DinnerApplication.class, args);
 	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//单个文件最大
+		factory.setMaxFileSize("10240KB"); //KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("102400KB");
+		return factory.createMultipartConfig();
+	}
+
 
 }
 
